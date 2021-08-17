44d411d7d9a5643cd84de476826ff4e36365ec2e
KirillTitarcukSoft <88536791+KirillTitarcukSoft@users.noreply.github.com>
2021-08-17 13:12:43 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -1,9 +1,20 @@
 <?php
 
 
-if ( $_POST['SubmitButton'] )
-{
-	exit("Check Login: $_POST[login]");
+if ( $_POST ['SubmitButton'] ) {
+	if ( !$_POST['g-recaptcha-respone'] )
+		exit('Please, Enter reCaptcha')
+	
+	$url = 'https://www.google.com/recaptcha/api/siteverify'
+	$key = '6LeB__MbAAAAAKV6evAHlmdjBw29suzTIJF1e9Ta'
+	$query = $url, '?secret=',key,'&response=',$_POST['g-recaptcha-respone'],'remoteip=',$_SERVER['REMOTE_ADDR'];
+	
+	$data = json_decode(file_get_contents($query));
+	
+	if ( $data->success = false)
+		exit('Enter reCaptcha incorrectly')
+	
+	exit("Login: $_POST[login]");
 }
 
 
